1072c535d40705e8b5a6ec721b2676eb2fdb9d5b
Lucas Sirignano <calin@Mac.lan>
2024-10-07 13:18:01 +0200

    install script

---
+++ p1/install_gns3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip python3-pyqt5 qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst bridge-utils
+sudo add-apt-repository ppa:gns3/ppa
+sudo apt-get update
+sudo apt-get install -y gns3-gui gns3-server
+sudo usermod -aG ubridge $USER
+sudo usermod -aG libvirt $USER
+sudo usermod -aG wireshark $USER
+newgrp ubridge
+newgrp libvirt
+newgrp wireshark
+#    sudo apt-get install -y docker.io
+#    sudo systemctl start docker
+#    sudo systemctl enable docker
+#    sudo usermod -aG docker $USER
+#    newgrp docker
