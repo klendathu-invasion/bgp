a668406c6569160bca933b298f379248f97565d3
Lucas Sirignano <calin@Mac.lan>
2024-10-08 10:06:24 +0200

    add some informations

--- p2/conf_static.sh
+++ p2/conf_static.sh
@@ -8,27 +8,27 @@ containers=$(docker ps -q)
 for c_id in ${containers[@]}; do
 	hostname=$(docker exec $c_id hostname)
 	if [[ $hostname == "host_lsirigna-1" ]]; then
-		docker exec $c_id sh -c "ip addr add 30.1.1.1/24 dev eth1" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
+		docker exec $c_id sh -c "ip addr add 30.1.1.1/24 dev eth1" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
 	elif [[ $hostname == "host_lsirigna-2" ]]; then
-		docker exec $c_id sh -c "ip addr add 30.1.1.2/24 dev eth1" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
+		docker exec $c_id sh -c "ip addr add 30.1.1.2/24 dev eth1" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
 	elif [[ $hostname == "router_lsirigna-1" ]]; then
-		docker exec $c_id sh -c "ip addr add 10.1.1.1/24 dev eth0" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip addr add 30.1.1.3/24 dev eth1" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.1 remote 10.1.1.2 dstport 4789 dev eth0" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link add br0 type bridge" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link set dev br0 up" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "brctl addif br0 eth1" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "brctl addif br0 vxlan10" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
+		docker exec $c_id sh -c "ip addr add 10.1.1.1/24 dev eth0" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip addr add 30.1.1.3/24 dev eth1" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.1 remote 10.1.1.2 dstport 4789 dev eth0" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link add br0 type bridge" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link set dev br0 up" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "brctl addif br0 eth1" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "brctl addif br0 vxlan10" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
 	elif [[ $hostname == "router_lsirigna-2" ]]; then
-		docker exec $c_id sh -c "ip addr add 10.1.1.2/24 dev eth0" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip addr add 30.1.1.4/24 dev eth1" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.2  remote 10.1.1.1 dstport 4789 dev eth0" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link add br0 type bridge" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "ip link set dev br0 up" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "brctl addif br0 eth1" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
-		docker exec $c_id sh -c "brctl addif br0 vxlan10" && echo '${GREEN}OK${END}' || echo '${RED}KO${END}'
+		docker exec $c_id sh -c "ip addr add 10.1.1.2/24 dev eth0" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip addr add 30.1.1.4/24 dev eth1" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.2  remote 10.1.1.1 dstport 4789 dev eth0" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link add br0 type bridge" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "ip link set dev br0 up" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "brctl addif br0 eth1" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
+		docker exec $c_id sh -c "brctl addif br0 vxlan10" && echo "${GREEN}OK${END}" || echo "${RED}KO${END}"
 	fi
 done
 
