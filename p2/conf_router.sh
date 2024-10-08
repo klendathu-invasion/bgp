079e27876b7313695cb85e5885e4ba71a687b739
Lucas Sirignano <calin@Mac.lan>
2024-10-08 09:51:06 +0200

    P2 started

--- p2/conf_router.sh
+++ p2/conf_router.sh
@@ -1,29 +1,60 @@
+#!/bin/bash
+
+containers=$(docker ps -q)
+for c_id in ${containers[@]}; do
+	hostname=$(docker exec $c_id hostname)
+	if [[ $hostname == "host_lsirigna-1" ]]; then
+		docker exec $c_id sh -c "ip addr add 30.1.1.1/24 dev eth1" && echo 'OK' || echo 'KO'
+	elif [[ $hostname == "host_lsirigna-2" ]]; then
+		docker exec $c_id sh -c "ip addr add 30.1.1.2/24 dev eth1" && echo 'OK' || echo 'KO'
+	elif [[ $hostname == "router_lsirigna-1" ]]; then
+		docker exec $c_id sh -c "ip addr add 10.1.1.1/24 dev eth0" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip addr add 30.1.1.3/24 dev eth1" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.1 remote 10.1.1.2 dstport 4789 dev eth0" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link add br0 type bridge" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link set dev br0 up" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "brctl addif br0 eth1" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "brctl addif br0 vxlan10" && echo 'OK' || echo 'KO'
+	elif [[ $hostname == "router_lsirigna-2" ]]; then
+		docker exec $c_id sh -c "ip addr add 10.1.1.2/24 dev eth0" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip addr add 30.1.1.4/24 dev eth1" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.2  remote 10.1.1.1 dstport 4789 dev eth0" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link add br0 type bridge" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "ip link set dev br0 up" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "brctl addif br0 eth1" && echo 'OK' || echo 'KO'
+		docker exec $c_id sh -c "brctl addif br0 vxlan10" && echo 'OK' || echo 'KO'
+	fi
+done
+
+
 # Bridge creation on router-1
-ip addr add 10.1.1.1/24 dev eth0
-ip addr add 30.1.1.3/24 dev eth1
-ip link add name vxlan10 type vxlan id 10 local 10.1.1.1 remote 10.1.1.2 dstport 4789 dev eth0
-ip link set dev vxlan10 up
-ip link add br0 type bridge
-ip link set dev br0 up
-brctl addif br0 eth1
-brctl addif br0 vxlan10
+# ip addr add 10.1.1.1/24 dev eth0
+# ip addr add 30.1.1.3/24 dev eth1
+# ip link add name vxlan10 type vxlan id 10 local 10.1.1.1 remote 10.1.1.2 dstport 4789 dev eth0
+# ip link set dev vxlan10 up
+# ip link add br0 type bridge
+# ip link set dev br0 up
+# brctl addif br0 eth1
+# brctl addif br0 vxlan10
 
-# Bridge creation on router-2
-ip addr add 10.1.1.2/24 dev eth0
-ip addr add 30.1.1.4/24 dev eth1
-ip link add name vxlan10 type vxlan id 10 local 10.1.1.2  remote 10.1.1.1 dstport 4789 dev eth0
-ip link set dev vxlan10 up
-ip link add br0 type bridge
-ip link set dev br0 up
-brctl addif br0 eth1
-brctl addif br0 vxlan10
+# # Bridge creation on router-2
+# ip addr add 10.1.1.2/24 dev eth0
+# ip addr add 30.1.1.4/24 dev eth1
+# ip link add name vxlan10 type vxlan id 10 local 10.1.1.2  remote 10.1.1.1 dstport 4789 dev eth0
+# ip link set dev vxlan10 up
+# ip link add br0 type bridge
+# ip link set dev br0 up
+# brctl addif br0 eth1
+# brctl addif br0 vxlan10
 
-#configure host-1
-ip addr add 30.1.1.1/24 dev eth1
+# #configure host-1
+# ip addr add 30.1.1.1/24 dev eth1
 
-#configure host-2
-ip addr add 30.1.1.2/24 dev eth1
+# #configure host-2
+# ip addr add 30.1.1.2/24 dev eth1
 
 #to show configs
-ip -d link show vxlan10
-ip addr show eth0
+# ip -d link show vxlan10
+# ip addr show eth0
