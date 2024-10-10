a7b0c666649c521367b9445483c807766e07aae2
Lucas Sirignano <calin@mac.home>
2024-10-10 11:17:40 +0200

    add info

--- p3/show_mac_and_ip.sh
+++ p3/show_mac_and_ip.sh
@@ -3,24 +3,30 @@
 END='\033[0m'
 RED='\033[1;91m'
 GREEN='\033[1;92m'
+YELLOW='\033[1;93m'
 
 containers=$(docker ps -q)
 for c_id in ${containers[@]}; do
 	hostname=$(docker exec $c_id hostname)
 	if [[ $hostname == "host_lsirigna-1" ]]; then
-		echo "$hostname :" 
+		echo "${GREEN}$hostname : ${END}"
 		docker exec $c_id sh -c "ip a | grep eth1 | grep inet | cut -d ' ' -f 6"
 	elif [[ $hostname == "host_lsirigna-2" ]]; then
+		echo "${GREEN}$hostname : ${END}"
 		docker exec $c_id sh -c "ip a | grep eth1 | grep inet | cut -d ' ' -f 6"
 	elif [[ $hostname == "host_lsirigna-3" ]]; then
+		echo "${GREEN}$hostname : ${END}"
 		docker exec $c_id sh -c "ip a | grep eth1 | grep inet | cut -d ' ' -f 6"
 	elif [[ $hostname == "router_lsirigna-1" ]]; then
+		echo "${YELLOW}$hostname : ${END}"
 		docker exec $c_id sh -c "ip a | grep eth1 | grep inet | cut -d ' ' -f 6"
 		docker exec $c_id sh -c "ip a | grep eth0 | grep inet | cut -d ' ' -f 6"
 	elif [[ $hostname == "router_lsirigna-2" ]]; then
+		echo "${YELLOW}$hostname : ${END}"
 		docker exec $c_id sh -c "ip a | grep eth1 | grep inet | cut -d ' ' -f 6"
 		docker exec $c_id sh -c "ip a | grep eth0 | grep inet | cut -d ' ' -f 6"
 	elif [[ $hostname == "router_lsirigna-3" ]]; then
+		echo "${YELLOW}$hostname : ${END}"
 		docker exec $c_id sh -c "ip a | grep eth1 | grep inet | cut -d ' ' -f 6"
 		docker exec $c_id sh -c "ip a | grep eth0 | grep inet | cut -d ' ' -f 6"
 	fi
