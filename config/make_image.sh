a3e3e5630e8f79df6531a835fc3fd00aeaa5c600
Lucas Sirignano <calin@MacBook-Pro.local>
2024-10-23 10:26:58 +0200

    change name

--- config/make_image.sh
+++ config/make_image.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-docker build -t host_lsirigna -f host_image .
-docker build -t router_lsirigna -f router_image .
+docker build -t host_lsirign -f host_image .
+docker build -t router_lsirign -f router_image .
 
+
+docker build -t host_ael-kadh -f host_image .
+docker build -t router_ael-kadh -f router_image .
+
+docker build -t host_smarie-c -f host_image .
+docker build -t router_smarie-c -f router_image .
--- p2/conf_static.sh
+++ p2/conf_static.sh
@@ -7,11 +7,11 @@ GREEN='\033[1;92m'
 containers=$(docker ps -q)
 for c_id in ${containers[@]}; do
 	hostname=$(docker exec $c_id hostname)
-	if [[ $hostname == "host_lsirigna-1" ]]; then
+	if [[ $hostname == "host_lsirign-1" ]]; then
 		docker exec $c_id sh -c "ip addr add 30.1.1.1/24 dev eth1" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
-	elif [[ $hostname == "host_lsirigna-2" ]]; then
+	elif [[ $hostname == "host_lsirign-2" ]]; then
 		docker exec $c_id sh -c "ip addr add 30.1.1.2/24 dev eth1" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
-	elif [[ $hostname == "router_lsirigna-1" ]]; then
+	elif [[ $hostname == "router_lsirign-1" ]]; then
 		docker exec $c_id sh -c "ip addr add 10.1.1.1/24 dev eth0" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
 		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.1 remote 10.1.1.2 dstport 4789 dev eth0" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
 		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
@@ -19,7 +19,7 @@ for c_id in ${containers[@]}; do
 		docker exec $c_id sh -c "ip link set dev br0 up" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
 		docker exec $c_id sh -c "brctl addif br0 eth1" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
 		docker exec $c_id sh -c "brctl addif br0 vxlan10" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
-	elif [[ $hostname == "router_lsirigna-2" ]]; then
+	elif [[ $hostname == "router_lsirign-2" ]]; then
 		docker exec $c_id sh -c "ip addr add 10.1.1.2/24 dev eth0" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
 		docker exec $c_id sh -c "ip link add name vxlan10 type vxlan id 10 local 10.1.1.2  remote 10.1.1.1 dstport 4789 dev eth0" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
 		docker exec $c_id sh -c "ip link set dev vxlan10 up" && echo -e "${GREEN}OK${END}" || echo -e "${RED}KO${END}"
